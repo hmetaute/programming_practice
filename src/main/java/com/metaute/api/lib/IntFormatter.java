fb6dd0a88604ec02ce61d54731964b9642fa8d62
Hernan Metaute <hmetaute@cvent.com>
2019-06-05 17:42:22 -0500

    Refactored digits into a map getting rid of the switch statement

--- src/main/java/com/metaute/api/lib/IntFormatter.java
+++ src/main/java/com/metaute/api/lib/IntFormatter.java
@@ -12,7 +12,16 @@ public class IntFormatter {
 
     public IntFormatter() {
         this.units = new HashMap<>();
+        units.put(0, "zero");
         units.put(1, "one");
+        units.put(2, "two");
+        units.put(3, "three");
+        units.put(4, "four");
+        units.put(5, "five");
+        units.put(6, "six");
+        units.put(7, "seven");
+        units.put(8, "eight");
+        units.put(9, "nine");
     }
 
     /**
@@ -22,26 +31,8 @@ public class IntFormatter {
      */
     public String format(int toTransform) {
         String formattedNumber;
-        if (toTransform == 0) {
-            formattedNumber = "zero";
-        } else if (toTransform == 1) {
-            formattedNumber = "one";
-        } else if (toTransform == 2) {
-            formattedNumber = "two";
-        } else if (toTransform == 3) {
-            formattedNumber = "three";
-        } else if (toTransform == 4) {
-            formattedNumber = "four";
-        } else if (toTransform == 5) {
-            formattedNumber = "five";
-        } else if (toTransform == 6) {
-            formattedNumber = "six";
-        } else if (toTransform == 7) {
-            formattedNumber = "seven";
-        } else if (toTransform == 8) {
-            formattedNumber = "eight";
-        } else if (toTransform == 9) {
-            formattedNumber = "nine";
+        if (toTransform < 10) {
+            formattedNumber = this.units.get(toTransform);
         } else {
             formattedNumber = "???";
         }
