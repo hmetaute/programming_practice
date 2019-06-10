ab3ce188c0d59edecc1b8c3a217e7556eca2c4b8
Hernan Metaute <hmetaute@cvent.com>
2019-06-09 21:58:37 -0500

    Sanitizing input before trying to parse it

--- src/main/java/com/metaute/api/main/Main.java
+++ src/main/java/com/metaute/api/main/Main.java
@@ -4,7 +4,6 @@ import com.metaute.api.lib.IntFormatter;
 
 import java.nio.charset.StandardCharsets;
 import java.util.InputMismatchException;
-import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -13,31 +12,38 @@ import java.util.Scanner;
  */
 public class Main {
 
+    private static final CharSequence[] specialCharactersToIgnore = { ".", ",", "`", "-", " " };
+
+    /**
+     * Tries to remove special separator characters from an input String
+     * @param input
+     * @return
+     */
+    private static String sanitizeInputString(String input) {
+        String result = input;
+        for (int i = 0; i < specialCharactersToIgnore.length; i++) {
+            result = result.replace(specialCharactersToIgnore[i], "");
+        }
+        return result;
+    }
+
 
 
     public static void main(String[] args) {
         System.out.println("Welcome to the integer formatter! You can convert Integers to Strings in english");
         System.out.println("In order to stop the program, press CTRL + C.");
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
-
+        System.out.println("You can format numbers between " + Integer.MIN_VALUE + " and " + Integer.MAX_VALUE);
         IntFormatter formatter = new IntFormatter();
         while(true) {
             System.out.println("Enter the number you wish to convert.");
-            System.out.println("");
+
             try {
-                int min = 1000000000;
-                int max = Integer.MAX_VALUE;
-                for(int i = 0; i < 100; i++) {
-                    Random random = new Random();
-                    int number = random.nextInt(max + 1 -min) + min;
-                    System.out.println(number);
-                }
                 Scanner input = new Scanner(System.in, StandardCharsets.UTF_8.name());
-                int intToTransform = input.nextInt();
+                String line = input.next();
+                int intToTransform = Integer.parseInt(sanitizeInputString(line));
                 String formattedInt = formatter.format(intToTransform);
                 System.out.println(formattedInt);
-            } catch (InputMismatchException e) {
+            } catch (NumberFormatException e) {
                 System.out.println("Your input couldn't be interpreted as a valid Int");
             }
         }
